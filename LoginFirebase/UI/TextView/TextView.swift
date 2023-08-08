5e413fb6478631a42b7cc9f5ca303b5f11a52144
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-08-08 13:11:53 -0300

    feat(TextView): created init with 'InputFieldType' argument.

--- LoginFirebase/UI/TextView/TextView.swift
+++ LoginFirebase/UI/TextView/TextView.swift
@@ -7,6 +7,20 @@
 
 import UIKit
 
+enum InputFieldType {
+    case username, password
+}
+
 class TextView: UIView {
+    init(type: InputFieldType) {
+        super.init(frame: .zero)
+        
+        backgroundColor = .secondarySystemBackground
+        layer.cornerRadius = 12
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.systemGray2.cgColor
+    }
+    
+    required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
     
 }
