07a8cefc2568506eb5f05bdcde7ea1e3561529f1
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-19 17:04:57 -0300

    feat(ViewController): add 'showAlert' method

--- LoginFirebase/View/ViewController.swift
+++ LoginFirebase/View/ViewController.swift
@@ -115,3 +115,11 @@ extension ViewController: UIGestureRecognizerDelegate {
         return touch.view === self.view
     }
 }
+
+extension ViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(alert, animated: true)
+    }
+}
