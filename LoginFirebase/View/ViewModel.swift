c20d5343be6004aca14dffded4a433abad9064c7
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-19 17:07:29 -0300

    feat(ViewModel): add credential validation method

--- LoginFirebase/View/ViewModel.swift
+++ LoginFirebase/View/ViewModel.swift
@@ -1,5 +1,40 @@
 import Foundation
 
-class ViewModel {
+enum AuthError: Error, LocalizedError {
+    case emptyUsername, emptyPassword
+    case shortUsername, shortPassword
     
+    public var errorDescription: String? {
+        switch self {
+        case .emptyUsername:
+            return NSLocalizedString("Username is empty", comment: "Invalid Username")
+        case .shortUsername:
+            return NSLocalizedString("Username has less than 4 characters", comment: "Invalid Username")
+        case .emptyPassword:
+            return NSLocalizedString("Password is empty", comment: "Invalid Password")
+        case .shortPassword:
+            return NSLocalizedString("Password has less than 4 characters", comment: "Invalid Password")
+        }
+    }
+}
+
+struct ViewModel {
+    public func validateCredentials(username: String, password: String) throws {
+        do {
+            try validateUsername(username)
+            try validatePassword(password)
+        } catch {
+            throw error
+        }
+    }
+    
+    private func validateUsername(_ username: String) throws {
+        if username.first == nil { throw AuthError.emptyUsername }
+        if username.count < 4 { throw AuthError.shortUsername }
+    }
+    
+    private func validatePassword(_ password: String) throws {
+        if password.first == nil { throw AuthError.emptyPassword }
+        if password.count < 4 { throw AuthError.shortPassword }
+    }
 }
