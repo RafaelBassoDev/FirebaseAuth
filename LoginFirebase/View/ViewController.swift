36a7f502d5e7d69916d8d3117396e5fa6077c379
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-19 15:15:27 -0300

    feat(ViewController): add log in button

--- LoginFirebase/View/ViewController.swift
+++ LoginFirebase/View/ViewController.swift
@@ -11,6 +11,20 @@ class ViewController: UIViewController {
 
     private let usernameField = TextField()
     private let passwordField = TextField(isSecure: true)
+
+    private lazy var logInButton: UIButton = {
+        var config: UIButton.Configuration = .filled()
+        config.background.backgroundColor = .systemBlue
+        config.baseForegroundColor = .white
+        config.title = "Log in"
+        config.cornerStyle = .capsule
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private var usernameFieldTopAnchor: NSLayoutConstraint!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -43,6 +57,25 @@ class ViewController: UIViewController {
             passwordField.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
             passwordField.heightAnchor.constraint(equalToConstant: 60)
         ])
+        
+        view.addSubview(logInButton)
+        NSLayoutConstraint.activate([
+            logInButton.topAnchor.constraint(equalTo: passwordField.bottomAnchor, constant: 15),
+            logInButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 12),
+            logInButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -12),
+            logInButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+
+        usernameFieldTopAnchor = usernameField.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -50)
+        usernameFieldTopAnchor.isActive = true
+
+        logInButton.addTarget(self, action: #selector(didClickLogInButton), for: .touchUpInside)
+        
+    }
+    
+    @objc
+    private func didClickLogInButton() {
+        
     }
     
     @objc
@@ -51,6 +84,7 @@ class ViewController: UIViewController {
             passwordField.didClickOffView()
         }
         usernameField.didClickOnView()
+        usernameFieldTopAnchor.constant = -100
     }
 
     @objc
@@ -59,12 +93,14 @@ class ViewController: UIViewController {
             usernameField.didClickOffView()
         }
         passwordField.didClickOnView()
+        usernameFieldTopAnchor.constant = -100
     }
     
     @objc
     private func fatherViewClicked() {
         if (usernameField.isFirstResponder) { usernameField.didClickOffView() }
         if (passwordField.isFirstResponder) { passwordField.didClickOffView() }
+        usernameFieldTopAnchor.constant = -50
     }
 
 }
