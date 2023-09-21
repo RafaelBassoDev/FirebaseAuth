faec909e2bd6bcb7e8be18b14297d6ce0eb50499
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-21 14:40:19 -0300

    feat: create file 'CreateAccountViewController'

---
+++ LoginFirebase/View/CreateAccount/CreateAccountViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CreateAccountViewController.swift
+//  LoginFirebase
+//
+//  Created by Rafael Basso on 21/09/23.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
