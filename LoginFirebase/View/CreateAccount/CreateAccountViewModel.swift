65f62cd650c516ea0f1ea73d8d5f405e415fb062
Rafael Basso <rafaelbasso2008@hotmail.com>
2023-09-23 12:41:35 -0300

    feat(CreateAccountViewModel): add validation service

--- LoginFirebase/View/CreateAccount/CreateAccountViewModel.swift
+++ LoginFirebase/View/CreateAccount/CreateAccountViewModel.swift
@@ -8,5 +8,17 @@
 import Foundation
 
 class CreateAccountViewModel {
+    private let validationService: ValidationService
     
+    init(service validationService: ValidationService = ValidationService()) {
+        self.validationService = validationService
+    }
+    
+    public func validateCredentials(username: String, password: String) throws {
+        do {
+            try validationService.validateCredentials(username: username, password: password)
+        } catch {
+            throw error
+        }
+    }
 }
